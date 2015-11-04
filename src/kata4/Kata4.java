dd0eaab840627ed8aa3958fa38db2f785814a6d1
Pablo Armas Martín <pabloarmasm@gmail.com>
2015-11-04 09:29:09 +0000

    Kata 4 versión 2 byte a byte .

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,10 +1,18 @@
 package kata4;
 
+import static com.oracle.jrockit.jfr.Transition.From;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
         String name = "/Users/Loedded";
         File file = new File (name);
@@ -16,7 +24,28 @@ public class Kata4 {
             //System.out.println(files);
         //}
         
-        print(file.listFiles(),"");
+        //print(file.listFiles(),"");
+    
+        String from = "/Users/Loedded/Desktop/CC-14-30.pdf";
+        String to = "/Users/Loedded/Desktop/CC-14-31.pdf";
+        
+        
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(new File (from)));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        
+        
+        Date date = new Date();
+        while (true){
+            int read = input .read();
+            if(read < 0) break;
+            output.write(read);
+        }
+        
+        input.close();
+        output.flush();
+        output.close();
+        
+        System.out.println(new Date().getTime() - date.getTime()+ " msg");
     }
 
     private static void print(File[] listFiles, String ident) {
